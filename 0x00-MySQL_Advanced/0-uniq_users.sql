54612c7872cad981db81b65d69a4b780d085bd4d
Terry <terrybmochire@gmail.com>
2023-01-12 00:25:05 +0300

    Task 0.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,8 @@
--- Creates a table with unique users
--- Attributes(id, name, email)
-
+-- script that creates a table users with following fields
+-- id, email, name
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     PRIMARY KEY (id)
 );
