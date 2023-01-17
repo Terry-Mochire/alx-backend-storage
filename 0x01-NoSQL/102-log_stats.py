92316aeff0d4e0c2e79d402eb035ab51e83a3e0b
Terry <terrybmochire@gmail.com>
2023-01-17 23:28:30 +0300

    Advanced task.

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 
 """
-a Python function that returns all students sorted by average score
+Improve 12-log_stats.py by adding the top 10 of the most present IPs in the collection nginx of the database logs
 """
+from pymongo import MongoClient
 
-import pymongo
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017')
+    collection = client.logs.nginx
 
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print("\tmethod {}: {}".format(method, collection.count_documents({"method": method})))
+    print("{} status check".format(collection.count_documents({"path": "/status"})))
 
-def top_students(mongo_collection):
-    """
-    Returns all students sorted by average score
-    """
-    return mongo_collection.aggregate([
-        {"$project": {"name": "$name", "averageScore": {"$avg": "$topics.score"}}},
-        {"$sort": {"averageScore": -1}}
+    print("IPs:")
+    ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
     ])
+    for ip in ips:
+        print("\t{}: {}".format(ip.get("_id"), ip.get("count")))
